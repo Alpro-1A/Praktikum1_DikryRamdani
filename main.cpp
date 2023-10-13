0c35cc4d5d7d9d6c01222b1d2c601a2ae2523da9
DikryRamdani <147692617+DikryRamdani@users.noreply.github.com>
2023-10-13 13:24:15 +0700

    main.cpp

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ int main()
     cout << "Npm : 231063117013" << endl <<endl;
     cout << "Kelas : 1A" <<endl <<endl;
     cout << "Cita -cita : Be a billionare" <<endl <<endl;  //dan bisa juga hanya dengan tanda titik koma
-    cout << "Motivasi masuk informatika : karena tertarik aja \n\n";  // \n itu seperti endl; tapi hanya bisa didalam tanda kutip
+    cout << "Motivasi masuk informatika : karena tertarik aja" <<endl <<endl;  // \n itu seperti endl; tapi hanya bisa didalam tanda kutip
     cout << "----------------------------------------------------" << endl << endl;
     return 1;
 }
