923524da7c5c8130b7908dcea4253b31d8b247cf
DikryRamdani <147692617+DikryRamdani@users.noreply.github.com>
2023-10-13 13:17:21 +0700

    menambahkan file main.cpp

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "----------------------------------------------------" << endl <<endl;; // \n itu seperti endl; tapi hanya bisa didalam tanda kutip
+    cout << "Nama : Dikry Ramdani" << endl <<endl;  //cout = c output
+    cout << "Npm : 231063117013" << endl <<endl;
+    cout << "Kelas : 1A" <<endl <<endl;
+    cout << "Cita -cita : Be a billionare" <<endl <<endl;  //dan bisa juga hanya dengan tanda titik koma
+    cout << "Motivasi masuk informatika : karena tertarik aja \n\n";  // \n itu seperti endl; tapi hanya bisa didalam tanda kutip
+    cout << "----------------------------------------------------" << endl << endl;
+    return 1;
+}
